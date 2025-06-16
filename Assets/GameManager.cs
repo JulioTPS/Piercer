a59af879009907bbcdc915692865973898e6ae56
Júlio Tetsuji Pinheiro So <so.julio@hotmail.com>
2025-06-16 15:58:19 -0300

    fixing pivots wip

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
+[System.Serializable]
+public struct SpawnEntry
+{
+    public GameObject piece;
+    public Vector3 spawnOffset;
+}
+
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] piecesPrefabs;
+
+    public List<SpawnEntry> pieces;
     public Vector3 spawnPosition = new Vector3(-1, 20, 0);
     public int points = 0;
     public Time gameTime = new Time();
@@ -13,7 +22,11 @@ public class GameManager : MonoBehaviour
 
     void SpawnRandomPiece()
     {
-        Instantiate(piecesPrefabs[Random.Range(0, 6)], spawnPosition, Quaternion.identity);
+        if (pieces == null || pieces.Count == 0)
+            return;
+
+        var piceSelected = pieces[Random.Range(0, pieces.Count)];
+        Instantiate(piceSelected.piece, spawnPosition + piceSelected.spawnOffset, Quaternion.identity);
     }
 
     void Update()
