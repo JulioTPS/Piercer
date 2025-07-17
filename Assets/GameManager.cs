c5845ee05aaa3bd4e50dc4f93d497e4673d1da3a
Júlio Tetsuji Pinheiro So <so.julio@hotmail.com>
2025-07-17 17:44:35 -0300

    separated piece from logic

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,54 +1,11 @@
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class PieceHandler
-{
-    private readonly List<GameObject> pieceReferences;
-    private List<GameObject> bag;
-    private GameObject previousPiece;
-    private const float PIECE_REPEAT_CHANCE = 0.25f;
-    private GameObject keptPiece;
-
-    public PieceHandler(List<GameObject> pieceReferences)
-    {
-        this.pieceReferences = new List<GameObject>(pieceReferences);
-        FillBag();
-    }
-
-    private void FillBag()
-    {
-        bag = new List<GameObject>(pieceReferences);
-        do
-        {
-            for (int i = bag.Count - 1; i > 0; i--)
-            {
-                int randomIndex = Random.Range(0, i + 1);
-                (bag[randomIndex], bag[i]) = (bag[i], bag[randomIndex]);
-            }
-        } while (bag[0] == previousPiece && Random.value > PIECE_REPEAT_CHANCE);
-
-        previousPiece = bag[0];
-    }
-
-    public GameObject GetNextPiece()
-    {
-        if (bag.Count == 0)
-            FillBag();
-        GameObject piece = bag[0];
-        bag.RemoveAt(0);
-        return piece;
-    }
-}
-
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> pieces;
-    public Vector3 spawnPosition = new(0, 21, 0);
     public int score = 0;
     public static GameManager Instance;
-    public TextMeshPro scoreText;
-    private PieceHandler pieceHandler;
+    public TextMeshPro scoreTMPro;
 
     void Awake()
     {
@@ -56,7 +13,6 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            pieceHandler = new PieceHandler(pieces);
         }
         else
         {
@@ -64,23 +20,15 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void Start() { }
-
-    void SpawnRandomPiece()
+    void Start()
     {
-        var piceSelected = pieceHandler.GetNextPiece();
-        Instantiate(
-            piceSelected,
-            spawnPosition - piceSelected.transform.GetChild(0).GetComponent<Piece>().spawnOffset,
-            Quaternion.identity
-        );
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            SpawnRandomPiece();
+            AddScore(1);
         }
     }
 
@@ -88,6 +36,6 @@ public class GameManager : MonoBehaviour
     {
         score += _score * 100;
 
-        scoreText.text = "Score: " + score;
+        scoreTMPro.text = "Score: " + score;
     }
 }
